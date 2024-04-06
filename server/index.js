5224e6cfe795901d8e4628e622402243ff2b96c9
James Raj <84929538+JamesArockiaRaj@users.noreply.github.com>
2024-04-06 21:55:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,15 @@ const bycrypt = require('bcrypt')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://donation-app-frontend.vercel.app/"],
+        methods: ["POST","GET"],
+        credentials:true
+    }
+));
+
+app.use(express.json())
 
 // mongoose.connect("mongodb://localhost:27017/user");
 
